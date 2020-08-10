8be3eaf3605eeb59813a717614454d88f3f3f75b
unknown <you@example.com>
2020-08-10 09:22:27 +0300

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
+  { path: 'movies', loadChildren: () => import('./pages/movies/movies.module').then(m => m.MoviesModule) },
+  { path: 'music', loadChildren: () => import('./pages/music/music.module').then(m => m.MusicModule) },
+  { path: 'series', loadChildren: () => import('./pages/series/series.module').then(m => m.SeriesModule) },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

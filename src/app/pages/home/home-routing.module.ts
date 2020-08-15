c26d72fca5a0e8008cd861ae87d5796e24146a15
unknown <you@example.com>
2020-08-15 18:58:16 +0300

    test

--- src/app/pages/home/home-routing.module.ts
+++ src/app/pages/home/home-routing.module.ts
@@ -4,8 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home.component';
 
 const routes: Routes = [{ path: '', component: HomeComponent }];
-///////////////********* */
-/***еуые****/
+
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]

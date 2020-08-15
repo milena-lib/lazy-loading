be66eff7a5180a6d155c8bfea0457ec62d5fe536
unknown <you@example.com>
2020-08-15 17:49:13 +0300

    change in home page

--- src/app/pages/home/home-routing.module.ts
+++ src/app/pages/home/home-routing.module.ts
@@ -5,7 +5,7 @@ import { HomeComponent } from './home.component';
 
 const routes: Routes = [{ path: '', component: HomeComponent }];
 ///////////////
-/****/
+/***еуые****/
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
